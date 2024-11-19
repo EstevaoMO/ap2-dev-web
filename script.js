84a512c4a6d4e790c2b7f29961f9f7ab5e7cabd8
EstevaoMO <estevaomoraes48@gmail.com>
2024-11-19 18:35:05 -0300

    atualizações na página principal e nova página dos atletas iniciada

--- script.js
+++ script.js
@@ -1,29 +1,7 @@
 const senha_correta = 'FLAMENGO'; // Adicionar a criptografia hash
 
-// Através do ENTER no input
-document.addEventListener('DOMContentLoaded', function() {
-    const inputSenha = document.getElementById("escrever-senha");
-
-    if (inputSenha) {
-        inputSenha.addEventListener('keydown', function(event) {
-            if (event.key === 'Enter') {
-                event.preventDefault();
-
-                const senha_inserida_enter = inputSenha.value;
-
-                if (senha_inserida_enter === senha_correta) {
-                    sessionStorage.setItem('login?', true);
-                    window.location.replace("atletas.html");
-                } else {
-                    alert('A senha inserida está incorreta!');
-                }
-            }
-        });
-
-    }
-});
-
-function verificarSenha(){
+// FUNÇÃO PARA VERIFICAR EFETUAR LOGIN CHAMADA NO onClick DO BOTÃO "ENTRAR"
+function efetuarLogin(){
     const senha_inserida = document.getElementById("escrever-senha").value;
     
     if (senha_inserida === senha_correta){
@@ -35,9 +13,22 @@ function verificarSenha(){
     }
 }
 
-// Logout
+// LER O INPUT E VERIFICAR A SENHA AO TECLAR "ENTER" DO TECLADO
+document.addEventListener('DOMContentLoaded', function() {
+    const inputSenha = document.getElementById("escrever-senha");
+
+    if (inputSenha) {
+        inputSenha.addEventListener('keydown', function(event) {
+            if (event.key === 'Enter'){
+                efetuarLogin();
+            }
+        });
+    }
+});
+
+// FAZER LOGOUT POR MEIO DO CLIQUE NO BOTÃO "SAIR"
 document.getElementById("logout").onclick = () => {
-    sessionStorage.removeItem("login?");
+    sessionStorage.setItem("login?", false);
     window.location.replace("index.html");
     alert("Saiu!");
 }
